04183131e031b2781445b5aac4142d88322fe84a
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2021-11-12 07:42:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const prefix = "+"; 
 const config = require("./config.json")
 client.on("ready", () => {
