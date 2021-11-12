2ab3e51e32c9c7c57bf321100fad45df9cc94773
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2021-11-12 07:53:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_WEBHOOKS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ] });
 const prefix = "+"; 
 const config = require("./config.json")
 client.on("ready", () => {
